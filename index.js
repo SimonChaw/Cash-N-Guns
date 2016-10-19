8c1253ecc85f8f01b0113db482f4cf681f50fc46
SimonChaw <chawblah@gmail.com>
2016-10-19 12:05:18 -0300

    Tinkering

--- index.js
+++ index.js
@@ -2,18 +2,59 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var messages = [];
-var clients = [];
+var games = [];
+
+function Game(){
+  var id;
+  var players = [];
+
+  this.init = function(){
+    this.id = makeid();
+  };
+
+  function makeid()
+  {
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for( var i=0; i < 20; i++ )
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      return text;
+  }
+};
+
+function Player(){
+  var nickname;
+  var id;
+
+}
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/creategame', function(req, res){
+  var game = new Game();
+  game.init();
+  games.push(game);
+  console.log(games);
+  res.redirect('game/' + game.id);
+});
+
+app.get('/game/:gameID', function(req, res){
+  res.sendFile(__dirname + '/game.html');
+});
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');s
+  console.log('a user connected');
+  console.log(socket.client.id);
   io.emit('message history', messages);
   socket.on('disconnect', function(){
     console.log('user disconnected');
